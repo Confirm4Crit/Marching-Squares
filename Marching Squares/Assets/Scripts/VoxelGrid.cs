d75593179e029bd35a5c647e1b9949104437c9e0
Confirm4Crit <miles_robson@yahoo.com>
2015-09-11 14:16:19 -0400

    Up to Stenciling

--- Marching Squares/Assets/Scripts/VoxelGrid.cs
+++ Marching Squares/Assets/Scripts/VoxelGrid.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class VoxelGrid : MonoBehaviour {
+[SelectionBase]
+public class VoxelGrid : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public int Resoultion;
+
+    public GameObject VoxelPrefab;
+    // ReSharper disable once NotAccessedField.Local
+    private bool[] _voxels;
+    private float _voxelSize;
+
+    private Material[] _voxelMaterials;
+    public void Initialize(int resolution, float size)
+    {
+        Resoultion = resolution;
+        _voxelSize = size / resolution;
+        _voxels = new bool[resolution * resolution];
+        _voxelMaterials = new Material[_voxels.Length];
+        for (int i = 0, y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++, i++)
+            {
+                CreateVoxel(i, x, y);
+            }
+        }
+        SetVoxelColor();
+    }
+    public void SetVoxel(int voxelX, int voxelY, bool b)
+    {
+        _voxels[voxelY * Resoultion + voxelX] = b;
+        SetVoxelColor();
+    }
+    private void SetVoxelColor()
+    {
+        for (int i = 0; i < _voxels.Length; i++)
+        {
+            _voxelMaterials[i].color = _voxels[i] ? Color.black : Color.white;
+        }
+    }
+
+    private void CreateVoxel(int i, int i1, int i2)
+    {
+        GameObject o = Instantiate(VoxelPrefab);
+        o.transform.parent = transform;
+        o.transform.localPosition = new Vector3((i1 + 0.5f) * _voxelSize, (i2 + 0.5f) * _voxelSize);
+        o.transform.localScale = Vector3.one * _voxelSize *.9f;
+        _voxelMaterials[i] = o.GetComponent<MeshRenderer>().material;
+    }
+
+   
 }
---
+++ Marching Squares/Assets/Scripts/VoxelMap.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class VoxelMap : MonoBehaviour {
+
+    public float Size = 2f;
+
+    public int VoxelResolution = 8;
+    public int ChunkResolution = 2;
+
+    public VoxelGrid VoxelGridPrefab;
+
+    private VoxelGrid[] _chunks;
+
+    private float _chunkSize, _voxelSize, _halfSize;
+
+    private void Awake()
+    {
+        _halfSize = Size * 0.5f;
+        _chunkSize = Size / ChunkResolution;
+        _voxelSize = _chunkSize / VoxelResolution;
+
+        _chunks = new VoxelGrid[ChunkResolution * ChunkResolution];
+        for (int i = 0, y = 0; y < ChunkResolution; y++)
+        {
+            for (int x = 0; x < ChunkResolution; x++, i++)
+            {
+                CreateChunk(i, x, y);
+            }
+        }
+        BoxCollider box = gameObject.AddComponent<BoxCollider>();
+        box.size = new Vector3(Size, Size);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo))
+            {
+                if (hitInfo.collider.gameObject == gameObject)
+                {
+                    EditVoxels(transform.InverseTransformPoint(hitInfo.point));
+                }
+            }
+        }
+    }
+
+    private void EditVoxels(Vector3 point)
+    {
+        int voxelX = (int)((point.x + _halfSize) / _voxelSize);
+        int voxelY = (int)((point.y + _halfSize) / _voxelSize);
+        int chunkX = voxelX / VoxelResolution;
+        int chunkY = voxelY / VoxelResolution;
+        voxelX -= chunkX * VoxelResolution;
+        voxelY -= chunkY * VoxelResolution;
+        _chunks[chunkY * ChunkResolution + chunkX].SetVoxel(voxelX, voxelY, true);
+        Debug.Log(voxelX + ", " + voxelY + " in chunk " + chunkX + ", " + chunkY);
+    }
+
+    private void CreateChunk(int i, int x, int y)
+    {
+        VoxelGrid chunk = Instantiate(VoxelGridPrefab);
+        chunk.Initialize(VoxelResolution, _chunkSize);
+        chunk.transform.parent = transform;
+        chunk.transform.localPosition = new Vector3(x * _chunkSize - _halfSize, y * _chunkSize - _halfSize);
+        _chunks[i] = chunk;
+    }
+}
